5a454be9c57f4f6c62c1f136a7516f7c76cf6010
BrandonDorsey1 <jbstrat85@gmail.com>
2022-06-30 18:30:57 -0400

    commit

---
+++ main (1).py
@@ -0,0 +1,47 @@
+# Title
+print("                   ~ American Racer ~")
+print()
+
+#Introduction
+print("The Story of an espiring Race car driver")
+print()
+print("Your an amatuer driver in the race car circuit and you want to be a professional driver on day." )
+print()
+print("You've just won your first amature race!")
+
+#Options
+print()
+print("There are three man waiting to talk to you outside the stadium: ")
+print("a. A sponsor, who needs someone to race for his brand.")
+print("b. A trainer would like to teach him how to become a professional driver. ")
+print("c. A local street racer that wants to have a conversation with you.")
+print("d. A guy in a black jacket with a trench coat was waiting to talk to you.")
+myoption = input("Enter who you like to speak with: ")
+
+#If Statement
+if(myoption == "a"):
+  print("You have to pass your training to become a professional driver")
+  myoption = input("Would you like to  proceed yes or no: ")
+  if(myoption == "yes"):
+    print("You've just completed your training and to your first professional race. Congratulations to your new career!!! ")
+  elif(myoption == "no"):
+     print("You will go back to the race car circuit. ")
+ 
+if(myoption == "b"):
+  print("I need to test out your math skills kid! If I have five cars in the parking lot and I removed three how many cars do I currently have in the parking lot? ")
+  if(myoption == )
+
+if(myoption == "c"):
+  print("The street racer wants to show you he knows how to race cars by doing so, you both get into a car wreck which ends your career. Gameover! ")
+
+if(myoption== "d"):
+  print("The guy in the trench coat offered you a thousand dollars to drive him across state lines. ")
+
+
+
+  
+  
+
+
+
+
